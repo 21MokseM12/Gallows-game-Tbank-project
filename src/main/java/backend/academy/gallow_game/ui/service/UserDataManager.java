30015adb9f85a03ec408fc838abbd09e4bcefbde
21MokseM12 <maksim.buynov.00@mail.ru>
2024-09-10 11:26:25 +0300

    добавлен класс - сервис, который отвечает за ввод и вывод данных в консоль

---
+++ src/main/java/backend/academy/gallow_game/ui/service/UserDataManager.java
@@ -0,0 +1,20 @@
+package backend.academy.gallow_game.ui.service;
+
+import java.util.Scanner;
+
+public class UserDataManager {
+
+    private final Scanner input;
+
+    public UserDataManager() {
+        this.input = new Scanner(System.in);
+    }
+
+    public void write(String data) {
+        System.out.print(data);
+    }
+
+    public String read() {
+        return input.nextLine();
+    }
+}
