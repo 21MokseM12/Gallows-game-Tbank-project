de26758d6809077afa801a793722c857bd13e263
21MokseM12 <maksim.buynov.00@mail.ru>
2024-09-10 11:27:30 +0300

    UserInterface - класс, который отвечает за пользовательский интерфейс, ввод и вывод сообщений на экран, вывод элементов виселицы на экран пользователю

---
+++ src/main/java/backend/academy/gallow_game/ui/UserInterface.java
@@ -0,0 +1,41 @@
+package backend.academy.gallow_game.ui;
+
+import backend.academy.gallow_game.ui.enums.Messages;
+import backend.academy.gallow_game.ui.service.UserDataManager;
+
+public class UserInterface {
+
+    private final UserDataManager dataManager;
+
+    public UserInterface() {
+        this.dataManager = new UserDataManager();
+    }
+
+    public void getGreeting() {
+        dataManager.write(Messages.GREETING.toString());
+    }
+
+    public void getMenu() {
+        dataManager.write(Messages.MENU.toString());
+    }
+
+    public void chooseMenuVariant() {
+        dataManager.write(Messages.CHOOSE_VARIANT.toString());
+    }
+
+    public void getRulesFormatted(final int countFails) {
+        dataManager.write(Messages.RULES.toString().formatted(countFails));
+    }
+
+    public void getExitMessage() {
+        dataManager.write(Messages.EXIT_MESSAGE.toString());
+    }
+
+    public void getErrorMessage() {
+        dataManager.write(Messages.ERROR_MESSAGE.toString());
+    }
+
+    public String read() {
+        return dataManager.read();
+    }
+}
