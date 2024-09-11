db423936d7d256e41ec9acbe3120e06e590ef844
21MokseM12 <maksim.buynov.00@mail.ru>
2024-09-11 17:46:26 +0300

    Main.class перемещен в директорию samples

---
+++ src/main/java/backend/academy/samples/Main.java
@@ -0,0 +1,10 @@
+package backend.academy.samples;
+
+public final class Main {
+    private Main() {
+        throw new UnsupportedOperationException("This is a utility class and cannot be instantiated");
+    }
+
+    public static void main(String[] args) {
+    }
+}
