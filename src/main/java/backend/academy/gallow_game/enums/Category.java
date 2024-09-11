7649e052cfe30279e5c7e983de9a9f26a99cb709
21MokseM12 <maksim.buynov.00@mail.ru>
2024-09-12 00:18:41 +0300

    добавлен enum, содержащий в себе каждую категорию слов: ее название и имя соответствующей директории в resources

---
+++ src/main/java/backend/academy/gallow_game/enums/Category.java
@@ -0,0 +1,27 @@
+package backend.academy.gallow_game.enums;
+
+public enum Category {
+
+    CITIES("Города", "cities"),
+
+    CLOTHES("Одежда", "clothes"),
+
+    EAT("Еда", "eat"),
+
+    SPORT("Спорт", "sport"),
+
+    TECHNIC("Техника и компьютеры", "technic");
+
+    private final String categoryNameRU;
+
+    private final String categoryNameEN;
+
+    Category(String nameRU, String nameEN) {
+        this.categoryNameRU = nameRU;
+        this.categoryNameEN = nameEN;
+    }
+
+    public String getCategoryRU() {return this.categoryNameRU;}
+
+    public String getCategoryEN() {return this.categoryNameEN;}
+}
