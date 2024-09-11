633827be3653bc4d7b8f4689ced05508f6c503bd
21MokseM12 <maksim.buynov.00@mail.ru>
2024-09-12 00:40:30 +0300

    добавлен enum для уровня сложности с названием уровня и именем файла в resources, соответствующем этому уровню

---
+++ src/main/java/backend/academy/gallow_game/enums/DifficultLevel.java
@@ -0,0 +1,19 @@
+package backend.academy.gallow_game.enums;
+
+public enum DifficultLevel {
+
+    EASY("Легкий", "easy.txt"), MEDIUM("Средний", "medium.txt"), HARD("Сложный", "hard.txt");
+
+    private final String diffLevelName;
+
+    private final String diffLevelFileName;
+
+    DifficultLevel(String level, String fileName) {
+        this.diffLevelName = level;
+        this.diffLevelFileName = fileName;
+    }
+
+    public String getFileName() {return this.diffLevelFileName;}
+
+    public String getLevelName() {return this.diffLevelName;}
+}
