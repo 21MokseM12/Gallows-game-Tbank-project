1ddda47508efae79b594621d98218763d85db7c6
21MokseM12 <maksim.buynov.00@mail.ru>
2024-09-12 12:54:58 +0300

    добавлены методы очистки экрана, выводе сообщений о выбранной категории и выбранном уровне сложности пользователю; поправлен метод вывода сообщения об ошибке

--- src/main/java/backend/academy/gallow_game/ui/UserInterface.java
+++ src/main/java/backend/academy/gallow_game/ui/UserInterface.java
@@ -1,5 +1,7 @@
 package backend.academy.gallow_game.ui;
 
+import backend.academy.gallow_game.enums.Category;
+import backend.academy.gallow_game.enums.DifficultLevel;
 import backend.academy.gallow_game.ui.enums.Messages;
 import backend.academy.gallow_game.ui.service.UserDataManager;
 
@@ -33,10 +35,13 @@ public class UserInterface {
 
     public void getErrorMessage() {
         dataManager.write(Messages.ERROR_MESSAGE.toString());
+        dataManager.write("\n\n");
     }
 
     public void clear() {
         //TODO
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
     }
 
     public void chooseDifficultLevel() {
@@ -45,6 +50,39 @@ public class UserInterface {
 
     public void chooseCategory() {
         dataManager.write(Messages.CATEGORY_MESSAGE.toString());
+    }
+
+    public void sessionBegin() {
+        dataManager.write(Messages.GAME_BEGIN.toString());
+        dataManager.write("\n");
+    }
+
+    public void getMessageOfChosenCategory(int category) {
+        String message = Messages.CHOSEN_CATEGORY.toString();
+        message = switch (category) {
+            case 1 -> message.concat(Category.CITIES.getCategoryRU());
+            case 2 -> message.concat(Category.CLOTHES.getCategoryRU());
+            case 3 -> message.concat(Category.EAT.getCategoryRU());
+            case 4 -> message.concat(Category.SPORT.getCategoryRU());
+            case 5 -> message.concat(Category.TECHNIC.getCategoryRU());
+            default -> message;
+        };
+
+        dataManager.write(message);
+        dataManager.write("\n");
+    }
+
+    public void getMessageOfChosenDiffLevel(int diffLevel) {
+        String message = Messages.CHOSEN_DIFF_LEVEL.toString();
+        message = switch (diffLevel) {
+            case 1 -> message.concat(DifficultLevel.EASY.getLevelName());
+            case 2 -> message.concat(DifficultLevel.MEDIUM.getLevelName());
+            case 3 -> message.concat(DifficultLevel.HARD.getLevelName());
+            default -> message;
+        };
+
+        dataManager.write(message);
+        dataManager.write("\n\n");
     }
 
     public void playAgainMessage() {dataManager.write(Messages.PLAY_AGAIN.toString());}
