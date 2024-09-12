40471bb20334fc851a795eb0ab60a2ec36fcd2ad
21MokseM12 <maksim.buynov.00@mail.ru>
2024-09-12 12:57:08 +0300

    получение рандомной категории слова и уровня сложности вынесены в отдельные методы

--- src/main/java/backend/academy/gallow_game/service/Randomizer.java
+++ src/main/java/backend/academy/gallow_game/service/Randomizer.java
@@ -10,11 +10,15 @@ public class Randomizer {
     private final Random random = new Random();
 
     public String getRandomWord(int category, int diffLevel) throws WordNotFoundException {
-        if (category == 6) category = random.nextInt(1, category);
-        if (diffLevel == 4) diffLevel = random.nextInt(1, diffLevel);
-
         List<String> wordList = FileWordsManager.getWordList(category, diffLevel);
-
         return wordList.get(random.nextInt(wordList.size()));
+    }
+
+    public String getRandomCategory() {
+        return String.valueOf(random.nextInt(1, 6));
+    }
+
+    public String getRandomDiffLevel() {
+        return String.valueOf(random.nextInt(1, 4));
     }
 }
