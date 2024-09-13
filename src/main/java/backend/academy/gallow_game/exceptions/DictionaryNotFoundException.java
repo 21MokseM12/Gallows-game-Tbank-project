b6f0be30de36bfd8d759ff93f9e4ad8c43ab242d
21MokseM12 <maksim.buynov.00@mail.ru>
2024-09-13 06:55:02 +0300

    добавлено исключение - обертка для ошибки нахождения файла словаря

---
+++ src/main/java/backend/academy/gallow_game/exceptions/DictionaryNotFoundException.java
@@ -0,0 +1,12 @@
+package backend.academy.gallow_game.exceptions;
+
+public class DictionaryNotFoundException extends Throwable{
+
+    public DictionaryNotFoundException() {super();}
+
+    public DictionaryNotFoundException(String message) {super(message);}
+
+    public DictionaryNotFoundException(Throwable cause) {super(cause);}
+
+    public DictionaryNotFoundException(String message, Throwable cause) {super(message, cause);}
+}
