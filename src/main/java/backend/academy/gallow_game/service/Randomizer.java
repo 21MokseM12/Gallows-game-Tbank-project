a274866068146ee5fbf78eba3a80d511bd3b4938
21MokseM12 <maksim.buynov.00@mail.ru>
2024-09-19 23:59:56 +0300

    вынос границ категории и уровня сложности в переменные

--- src/main/java/backend/academy/gallow_game/service/Randomizer.java
+++ src/main/java/backend/academy/gallow_game/service/Randomizer.java
@@ -9,16 +9,22 @@ public class Randomizer {
 
     private final Random random = new Random();
 
+    private static final int BEGIN_NUMBER_COUNTDOWN = 1;
+
+    private static final int END_NUMBER_COUNTDOWN_CATEGORY = 6;
+
+    private static final int END_NUMBER_COUNTDOWN_DIFF_LEVEL = 4;
+
     public String getRandomWord(int category, int diffLevel) throws WordNotFoundException {
         List<String> wordList = GameFilesManager.getWordList(category, diffLevel);
         return wordList.get(random.nextInt(wordList.size()));
     }
 
     public String getRandomCategory() {
-        return String.valueOf(random.nextInt(1, 6));
+        return String.valueOf(random.nextInt(BEGIN_NUMBER_COUNTDOWN, END_NUMBER_COUNTDOWN_CATEGORY));
     }
 
     public String getRandomDiffLevel() {
-        return String.valueOf(random.nextInt(1, 4));
+        return String.valueOf(random.nextInt(BEGIN_NUMBER_COUNTDOWN, END_NUMBER_COUNTDOWN_DIFF_LEVEL));
     }
 }
