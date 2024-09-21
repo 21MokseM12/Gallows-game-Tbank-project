46297caf1f8dbc329eac06c0b44fe539ff357787
21MokseM12 <maksim.buynov.00@mail.ru>
2024-09-22 00:45:37 +0300

    refactor: меню правил вынесено в отдельный метод feature: добавлена валидация ввода пользователя при вопросе о повторной игре

--- src/main/java/backend/academy/gallow_game/App.java
+++ src/main/java/backend/academy/gallow_game/App.java
@@ -30,27 +30,7 @@ public final class App {
                     SESSION.start();
                     break;
                 case "2":
-                    UI.getRulesFormatted(SESSION.countFails());
-
-                    while (!exitRulesFlag) {
-                        UI.chooseMenuVariant();
-                        String rulesMenuResponse = UI.read();
-
-                        switch (rulesMenuResponse) {
-                            case "1":
-                                SESSION.start();
-                                exitRulesFlag = true;
-                                break;
-                            case "2":
-                                UI.getExitMessage();
-                                exitRulesFlag = true;
-                                exitGameFlag = true;
-                                break;
-                            default:
-                                UI.getErrorMessage();
-                                break;
-                        }
-                    }
+                    getRules();
                     break;
                 case "3":
                     UI.getExitMessage();
@@ -61,19 +41,54 @@ public final class App {
                     break;
             }
 
-            if (!exitGameFlag) {
-                UI.playAgainMessage();
-                UI.chooseMenuVariant();
-                String anotherGameResponse = UI.read();
-
-                if (anotherGameResponse.equals("2")) {
-                    exitGameFlag = true;
-                    UI.getExitMessage();
-                } else {
-                    exitRulesFlag = false;
-                }
-            }
+            playAgain();
 
         } while (!exitGameFlag);
+    }
+
+    private static void getRules() {
+        UI.getRulesFormatted(SESSION.countFails());
+
+        while (!exitRulesFlag) {
+            UI.chooseMenuVariant();
+            String rulesMenuResponse = UI.read();
+
+            switch (rulesMenuResponse) {
+                case "1":
+                    SESSION.start();
+                    exitRulesFlag = true;
+                    break;
+                case "2":
+                    UI.getExitMessage();
+                    exitRulesFlag = true;
+                    exitGameFlag = true;
+                    break;
+                default:
+                    UI.getErrorMessage();
+                    break;
+            }
+        }
+    }
+
+    private static void playAgain() {
+        boolean validFlag = false;
+
+        while (!validFlag) {
+            UI.playAgainMessage();
+            UI.chooseMenuVariant();
+            String anotherGameResponse = UI.read();
+
+            switch (anotherGameResponse) {
+                case "1":
+                    validFlag = true;
+                    break;
+                case "2":
+                    exitGameFlag = true;
+                    validFlag = true;
+                    UI.getExitMessage();
+                    break;
+                default: UI.getErrorMessage();
+            }
+        }
     }
 }
