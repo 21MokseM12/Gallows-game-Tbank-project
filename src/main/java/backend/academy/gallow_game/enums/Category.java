a28692f359990817b39d2077777bebccc3c47c62
moksem <maksim.buynov.00@mail.ru>
2024-09-25 16:14:50 +0300

    feature: добавление метода определения категории по введенному числу в Category.java

--- src/main/java/backend/academy/gallow_game/enums/Category.java
+++ src/main/java/backend/academy/gallow_game/enums/Category.java
@@ -1,31 +1,41 @@
 package backend.academy.gallow_game.enums;
 
+import java.util.NoSuchElementException;
+import lombok.Getter;
+
+@Getter
 public enum Category {
 
-    CITIES("Города", "cities"),
+    CITIES("Города", "cities", "1"),
 
-    CLOTHES("Одежда", "clothes"),
+    CLOTHES("Одежда", "clothes", "2"),
 
-    EAT("Еда", "eat"),
+    EAT("Еда", "eat", "3"),
 
-    SPORT("Спорт", "sport"),
+    SPORT("Спорт", "sport", "4"),
 
-    TECHNIC("Техника и компьютеры", "technic");
+    TECHNIC("Техника и компьютеры", "technic", "5");
 
     private final String categoryNameRU;
 
     private final String categoryNameEN;
 
-    Category(String nameRU, String nameEN) {
+    private final String categoryMenuNum;
+
+    Category(String nameRU, String nameEN, String categoryMenuNum) {
         this.categoryNameRU = nameRU;
         this.categoryNameEN = nameEN;
+        this.categoryMenuNum = categoryMenuNum;
     }
 
-    public String getCategoryRU() {
-        return this.categoryNameRU;
-    }
-
-    public String getCategoryEN() {
-        return this.categoryNameEN;
+    public static Category valueOfMenuNumber(String number) {
+        return switch (number) {
+            case "1" -> Category.CITIES;
+            case "2" -> Category.CLOTHES;
+            case "3" -> Category.EAT;
+            case "4" -> Category.SPORT;
+            case "5" -> Category.TECHNIC;
+            default -> throw new NoSuchElementException();
+        };
     }
 }
