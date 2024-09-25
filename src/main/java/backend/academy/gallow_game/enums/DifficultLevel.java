07892c9f715a578b56baa48af7e715cb28741859
moksem <maksim.buynov.00@mail.ru>
2024-09-25 16:15:12 +0300

    feature: добавление метода определения уровня сложности по введенному числу в DifficultLevel.java

--- src/main/java/backend/academy/gallow_game/enums/DifficultLevel.java
+++ src/main/java/backend/academy/gallow_game/enums/DifficultLevel.java
@@ -1,23 +1,38 @@
 package backend.academy.gallow_game.enums;
 
+import java.util.NoSuchElementException;
+import lombok.Getter;
+
+@Getter
 public enum DifficultLevel {
 
-    EASY("Легкий", "easy.txt"), MEDIUM("Средний", "medium.txt"), HARD("Сложный", "hard.txt");
+    EASY("Легкий", "easy.txt", "1", 7),
+
+    MEDIUM("Средний", "medium.txt", "2", 5),
+
+    HARD("Сложный", "hard.txt", "3", 3);
 
     private final String diffLevelName;
 
     private final String diffLevelFileName;
 
-    DifficultLevel(String level, String fileName) {
+    private final String diffLevelMenuNum;
+
+    private final int hintFrontier;
+
+    DifficultLevel(String level, String fileName, String diffLevelMenuNum, int hintFrontier) {
         this.diffLevelName = level;
         this.diffLevelFileName = fileName;
+        this.diffLevelMenuNum = diffLevelMenuNum;
+        this.hintFrontier = hintFrontier;
     }
 
-    public String getFileName() {
-        return this.diffLevelFileName;
-    }
-
-    public String getLevelName() {
-        return this.diffLevelName;
+    public static DifficultLevel valueOfMenuNumber(String number) {
+        return switch (number) {
+            case "1" -> DifficultLevel.EASY;
+            case "2" -> DifficultLevel.MEDIUM;
+            case "3" -> DifficultLevel.HARD;
+            default -> throw new NoSuchElementException();
+        };
     }
 }
